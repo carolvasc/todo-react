62a36fd74afc0ccca543fd14bd528f592992704b
Carol Vasconcelos <cvsouza04@gmail.com>
2020-05-08 22:00:42 -0300

    Melhorias na listagem

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -12,13 +12,13 @@ const TodoList = props => {
         return list.map(todo => (
             <tr key={todo._id}>
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
-                <td>
-                    <IconButton style='success' icon='check' hide={todo.done}
-                        onClick={() => props.markAsDone(todo)}></IconButton>
-                    <IconButton style='warning' icon='undo' hide={!todo.done} 
-                        onClick={() => props.markAsPending(todo)}></IconButton>
-                    <IconButton style='danger' icon='trash-o' hide={!todo.done} 
-                        onClick={() => props.remove(todo)}></IconButton>
+                <td className='tableActions'>
+                    <IconButton style='success float-right' icon='check' tooltip="Finalizar" hide={todo.done}
+                        onClick={() => props.markAsDone(todo)} />
+                    <IconButton style='warning' icon='undo' tooltip="Voltar" hide={!todo.done} 
+                        onClick={() => props.markAsPending(todo)} />
+                    <IconButton style='danger' icon='trash-o' tooltip="Excluir" hide={!todo.done} 
+                        onClick={() => props.remove(todo)} />
                 </td>
             </tr>
         ))
@@ -26,12 +26,6 @@ const TodoList = props => {
 
     return (
         <table className='table'>
-            <thead>
-                <tr>
-                    <th>Descrição</th>
-                    <th className='tableActions'>Ações</th>
-                </tr>
-            </thead>
             <tbody>
                 {renderRows()}
             </tbody>
